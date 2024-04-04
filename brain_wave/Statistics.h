3c167fd8c4f15365d9de04552f72967abc4495d8
bplabz23 <138505485+bplabz23@users.noreply.github.com>
2024-04-04 17:11:02 -0500

    Add files via upload
    Added a Correlation Co-efficient function (tested)

--- brain_wave/Statistics.h
+++ brain_wave/Statistics.h
@@ -13,7 +13,7 @@ class Statistics
         // Methods - basic statistics
         float average(float* data_list, int data_size);
         float stdDeviation(float* data_list, const int data_size);
-        float zScore(float* data_list, const int data_size);
+        float zScore(float data, float mean, float std_dev);
 
         // Linear Regression - stuff for building an equation of the form y = mx + b
         float getLinearCorrelation(float* var_x, float* var_y, const int data_size);
